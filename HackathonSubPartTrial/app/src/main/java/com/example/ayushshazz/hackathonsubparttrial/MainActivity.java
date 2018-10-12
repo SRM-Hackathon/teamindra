eeb96647300e16a617e92eb050c3faa0ec199d4c
ayushshaz <38434351+ayushshaz@users.noreply.github.com>
2018-10-12 18:22:41 +0530

    Layout created for some parts

---
+++ HackathonSubPartTrial/app/src/main/java/com/example/ayushshazz/hackathonsubparttrial/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ayushshazz.hackathonsubparttrial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editprofile);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
